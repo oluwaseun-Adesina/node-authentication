75cbf11010968544e48c56f4ced16cf28ff71026
oluwaseun <oluwaseunadesina8@gmail.com>
2022-03-16 17:07:53 -0400

    Protecting Routes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoute = require('./routes/authRoutes');
-const cookieParser = require('cookie-parser');
-
+const cookieParser = require('cookie-parser');  
+const { requireAuth } = require('./middleware/authMiddleware');
 const app = express();
 const port = 3000;
 
@@ -27,7 +27,8 @@ mongoose.connect(dbURI, {useUnifiedTopology: true, useNewUrlParser: true})
 //routes 
 
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 
 app.use(authRoute)
 
+ 
